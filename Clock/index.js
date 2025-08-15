86844d50031daaf5d19cbcd35e5a0b6a7eebb35f
72 <72sevenzy2@gmail.com>
2025-08-15 08:34:34 -0800

    Update index.js

--- Clock/index.js
+++ Clock/index.js
@@ -3,9 +3,9 @@ setInterval(() => {
     htime = d.getHours();
     mtime = d.getMinutes();
     stime = d.getSeconds();
-    hrotation = 30*htime + mtime/2;
-    mrotation = 6*mtime;
-    srotation = 6*stime;
+    hrotation = Math.sin(6) * htime;
+    mrotation = Math.sin(6) * mtime;
+    srotation = Math.sin(6) * stime;
 
     hour.style.transform = `rotate(${hrotation}deg)`;
     minute.style.transform = `rotate(${mrotation}deg)`;
